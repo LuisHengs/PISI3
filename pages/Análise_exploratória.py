415edabe09049bd895c1e4a67c6925e304933f01
LuisHengs <luis.hengs@protonmail.com>
2023-11-01 17:21:34 -0300

    juntando AE

---
+++ pages/Análise_exploratória.py
@@ -0,0 +1,69 @@
+import streamlit as st
+
+
+
+
+st.title('Análise exploratória dos Dataframe.')
+st.subheader('Selecione o a anaálise que você quer ver.')
+
+def pagina1():
+    st.title("Página 1 - Tópico A")
+    st.write("Conteúdo relacionado ao tópico A")
+
+def pagina2():
+    st.title("Página 2 - Tópico B")
+    st.write("Conteúdo relacionado ao tópico B")
+
+def pagina3():
+    st.title("Página 3 - Tópico C")
+    st.write("Conteúdo relacionado ao tópico C")
+
+def pagina4():
+    st.title("Página 4 - Tópico D")
+    st.write("Conteúdo relacionado ao tópico D")
+
+pagina_selecionada = st.selectbox("Selecione uma página", ("Página 1", "Página 2", "Página 3", "Página 4"))
+
+if pagina_selecionada == "Página 1":
+    def st_display_sweetviz(report_html, width=1000, height=500):
+    report_file = codecs.open(report_html, 'r')
+    page = report_file.read()
+    components.html(page, width=width, height=height, scrolling=True)
+
+  def main():
+
+      selected_dataset = st.selectbox('Escolha o dataset:', ['Netflix', 'Amazon Prime'])
+
+      if selected_dataset == 'Netflix':
+          data = pd.read_parquet('data/netflix_titles.parquet')
+      elif selected_dataset == 'Amazon Prime':
+          data = pd.read_parquet('data/amazon_prime_titles.parquet')
+
+      menu = ["Pandas Profile", "Sweetviz"]
+      choice = st.selectbox("Escolha sua ferramenta de análise exploratória", menu)
+
+      if choice == "Pandas Profile":
+          st.subheader("Pandas Profile")
+          data_file = selected_dataset
+          if data_file is not None:
+              st.dataframe(data.head())
+              profile = ProfileReport(data)
+              st_profile_report(profile)
+
+      elif choice == "Sweetviz":
+          st.subheader("Sweetviz")
+          data_file = selected_dataset
+          if data_file is not None:
+              st.dataframe(data.head())
+              if st.button("Gerar relatorio Sweetviz"):
+                  # Normal Workflow
+                  report = sv.analyze(data)
+                  report.show_html()
+                  st_display_sweetviz("SWEETVIZ_REPORT.html")
+
+  if __name__ == '__main__':
+      main()
+elif pagina_selecionada == "Página 2":
+    pagina2()
+elif pagina_selecionada == "Página 3":
+    pagina3()
