a03e7e3832a154f733385bf81f4268f6351f6102
Pr0chips <149016713+Pr0chips@users.noreply.github.com>
2023-11-01 22:13:10 -0300

    Teste

--- pages/Análise_exploratória.py
+++ pages/Análise_exploratória.py
@@ -21,12 +21,10 @@ def pagina2():
 def pagina3():
     st.title("Análise do tempo de duração de filmes e séries.")
     
-
 def pagina4():
-    st.title("Página 4 - Tópico D")
-    st.write("Conteúdo relacionado ao tópico D")
+    st.title("Análise com base nos gêneros")
 
-pagina_selecionada = st.selectbox("Selecione uma análise", ("Análise geral", "Análise quantitativa de filmes e séries.", "Análise tempo de duração.", "Página 4"))
+pagina_selecionada = st.selectbox("Selecione uma análise", ("Análise geral", "Análise quantitativa de filmes e séries.", "Análise tempo de duração.", "Análise com base nos gêneros"))
 
 if pagina_selecionada == "Análise geral":
     
@@ -183,6 +181,65 @@ elif pagina_selecionada == "Análise tempo de duração.":
     elif selected_dataset == 'Amazon Prime':
         st.header('Duração dos Filmes no Amazon Prime')
         plot_duration(data_amazon_prime, 'Duração dos Filmes no Amazon Prime')
-elif pagina_selecionada == "Página 3":
-    pagina4()
-    #não esquecer do tab
+
+
+elif pagina_selecionada == "Análise com base nos gêneros":
+    @st.cache_data
+    def carregar_dataset_netflix():
+        data = pd.read_parquet("netflix_titles.parquet")
+        return data
+
+    @st.cache_data
+    def carregar_dataset_amazon():
+        data = pd.read_parquet("amazon_prime_titles.parquet")
+        return data
+
+    st.title("Distribuição de Filmes e Séries por Gênero")
+
+
+    selected_service = st.radio("Escolha o serviço de streaming", ["Netflix", "Amazon Prime"])
+
+
+    if selected_service == "Netflix":
+        df = carregar_dataset_netflix()
+    else:
+        df = carregar_dataset_amazon()
+
+
+    selected_chart = st.radio("Escolha o tipo de gráfico", ["Filmes", "Séries"])
+
+
+    if selected_chart == "Filmes":
+        chart_data = df[df["type"] == "Movie"]
+    else:
+        chart_data = df[df["type"] == "TV Show"]
+
+
+    genre_counts = chart_data["listed_in"].value_counts().reset_index()
+    genre_counts.columns = ["Gênero", f"Número de {selected_chart}"]
+    genre_counts = genre_counts.sort_values(by=f"Número de {selected_chart}", ascending=False)
+
+
+    outros_threshold = 10
+    top_genres = genre_counts.head(outros_threshold)
+    outros_genres = genre_counts.tail(len(genre_counts) - outros_threshold)
+    outros_row = pd.DataFrame(data={
+        "Gênero": ["Outros"],
+        f"Número de {selected_chart}": [outros_genres[f"Número de {selected_chart}"].sum()]
+    })
+    genre_counts = pd.concat([top_genres, outros_row])
+
+    fig = px.bar(genre_counts, x="Gênero", y=f"Número de {selected_chart}",
+                title=f"Distribuição de {selected_chart} por Gênero no {selected_service}",
+                color="Gênero", color_discrete_sequence=px.colors.qualitative.Set3)
+
+
+    fig.update_xaxes(tickangle=45, categoryorder="total ascending")
+
+
+    fig.update_xaxes(title_text="Gênero")
+    fig.update_yaxes(title_text=f"Número de {selected_chart}")
+
+
+
+    st.plotly_chart(fig)
