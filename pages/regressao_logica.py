d14c9bc7f2cef4c2ce7dab0e923caf75f09226ca
Vitu3301 <149022031+Vitu3301@users.noreply.github.com>
2024-02-28 01:42:45 -0300

    Função para carregar os dados

--- pages/regressao_logica.py
+++ pages/regressao_logica.py
@@ -6,3 +6,12 @@ from sklearn.preprocessing import LabelEncoder, StandardScaler
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
 from sklearn.metrics import accuracy_score, confusion_matrix
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+
+# Função para carregar os dados
+def load_data():
+    # Carregue seus dados aqui, por exemplo, a partir de um arquivo CSV
+    data = pd.read_parquet('dados_netflix_amazon_5.parquet')
+    return data
