3f93550630c2bfed7ffc98fabc67d84cf179c46b
Vitu3301 <149022031+Vitu3301@users.noreply.github.com>
2024-02-27 23:53:19 -0300

    imports

--- pages/regressao_logica.py
+++ pages/regressao_logica.py
@@ -1,1 +1,8 @@
-
+import streamlit as st
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.metrics import accuracy_score, confusion_matrix
